ded3c47decd929a1291d7b38a9d4c409123140cf
Aymen <sasorifr@gmail.com>
2025-09-02 19:53:00 +0200

    chore: duct taping meta descriptions for vercel deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import { type Metadata } from "next";
-
-import { asText } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
-
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 
 export default async function Home() {
   const client = createClient();
-  const home = await client.getByUID("page", "home");
+  const home = await client.getByUID("page", "homepage");
 
   // <SliceZone> renders the page's slices.
   return <SliceZone slices={home.data.slices} components={components} />;
@@ -16,14 +13,14 @@ export default async function Home() {
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const home = await client.getByUID("page", "home");
+  const home = await client.getByUID("page", "homepage");
 
-  return {
-    title: asText(home.data.title),
-    description: home.data.meta_description,
+  return { 
+    title: home.uid ?? "Homepage",
+    description: undefined,
     openGraph: {
-      title: home.data.meta_title ?? undefined,
-      images: [{ url: home.data.meta_image.url ?? "" }],
+      title: undefined,
+      images: undefined,
     },
   };
 }
